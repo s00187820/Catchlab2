0a1aa4848dc0d0fc1cbb678aaa891ad2da5dcf08
CianO'Reilly <46957507+GitSmugg@users.noreply.github.com>
2020-05-27 15:37:36 +0100

    Add project files.

---
+++ Catchlab2/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catchlab2
+{
+    static public class AudioManager
+    {
+        static public Dictionary<string, SoundEffect> SoundEffects
+            = new Dictionary<string, SoundEffect>();
+
+
+        public static void Play(ref SoundEffectInstance _playInstance, string sound)
+        {
+            if (_playInstance == null)
+            {
+                _playInstance = AudioManager.SoundEffects[sound].CreateInstance();
+                _playInstance.Play();
+            }
+            else if (_playInstance != null)
+            {
+                if (_playInstance.State == SoundState.Stopped)
+                {
+                    _playInstance.Dispose();
+                    _playInstance = null;
+                }
+            }
+        }
+    }
+}
---
+++ Catchlab2/Game1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Catchlab2
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SoundEffectInstance sf = null;
+        Dictionary<string, Texture2D> badges = new Dictionary<string, Texture2D>();
+
+        public Game1()
+        {
+            //Activity.Track("displaying badges and sound effect playing");
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            AudioManager.SoundEffects = Loader.ContentLoad<SoundEffect>(Content, "Audio");
+            AudioManager.Play(ref sf, "Badges_6");
+            base.Initialize();
+        }
+
+
+
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            badges = Loader.ContentLoad<Texture2D>(Content, "Badges");
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+
+
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.Draw(badges["Badges_0"], new Vector2(20, 20), Color.White);
+            spriteBatch.Draw(badges["Badges_1"], new Vector2(30, 130), Color.White);
+            spriteBatch.Draw(badges["Badges_2"], new Vector2(40, 240), Color.White);
+            spriteBatch.Draw(badges["Badges_3"], new Vector2(250, 50), Color.White);
+            spriteBatch.Draw(badges["Badges_4"], new Vector2(360, 60), Color.White);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
